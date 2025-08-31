19336ee591f214e9e422d649152595df129eeec7
Aditi <Aditi@dauntlesscoder.com>
2025-08-31 15:46:19 +0530

    backend

--- dripyard-backend/src/main/java/com/aditi/dripyard/controller/AuthController.java
+++ dripyard-backend/src/main/java/com/aditi/dripyard/controller/AuthController.java
@@ -2,24 +2,33 @@ package com.aditi.dripyard.controller;
 
 
 import com.aditi.dripyard.model.User;
+import com.aditi.dripyard.repository.UserRepository;
 import com.aditi.dripyard.response.SignupRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
 
+    private final UserRepository userRepository;
 
-    @PostMapping
+
+    @PostMapping ("/signup")
     public ResponseEntity<User>  createUserHandler(@RequestBody SignupRequest req){
 
         User user = new User();
         user.setEmail(req.getEmail());
         user.setFullName(req.getFullName());
 
+        User savedUser = userRepository.save(user);
 
-        return ResponseEntity.ok(user);
+
+        return ResponseEntity.ok(savedUser);
     }
 }
