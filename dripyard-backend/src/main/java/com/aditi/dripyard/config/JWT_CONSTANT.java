f65e15ae2b0c9909049c6ed87a824058f41b156a
Aditi <Aditi@dauntlesscoder.com>
2025-09-02 22:04:47 +0530

    backend

---
+++ dripyard-backend/src/main/java/com/aditi/dripyard/config/JWT_CONSTANT.java
@@ -0,0 +1,8 @@
+package com.aditi.dripyard.config;
+
+public class JWT_CONSTANT {
+
+    public static final String SECRET_KEY= "XYZ123";
+
+    public static final String  JWT_HEADER = "Authorization";
+}
