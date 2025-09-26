868bce0d9ef516ad3497d42504db90b7759607f1
Aditi <aditisharma9104@gmail.com>
2025-09-26 14:25:57 +0530

    railway

--- dripyard-backend/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Step 3a: Build the project
-./mvnw clean package -DskipTests
-
-# Step 3b: Run the Spring Boot jar
-java -jar target/dripyard-backend-0.0.1-SNAPSHOT.jar
---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Navigate to your backend folder
+cd dripyard-backend
+
+# Run Spring Boot jar
+java -jar target/dripyard-backend-0.0.1-SNAPSHOT.jar
