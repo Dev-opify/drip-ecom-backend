6f75c49dd4e564b8f6600237c0272cf1f21e8919
Aditi <aditisharma9104@gmail.com>
2025-09-26 15:02:18 +0530

    make start.sh executable

--- start.sh
+++ start.sh
@@ -1,7 +1,3 @@
-#!/bin/bash
-
-# Go to the folder where your jar is
-cd dripyard-backend/target
-
-# Run the Spring Boot jar with dynamic port
+#!/bin/sh
+# Make sure the jar is in the same folder as start.sh
 java -jar dripyard-backend-0.0.1-SNAPSHOT.jar --server.port=$PORT
