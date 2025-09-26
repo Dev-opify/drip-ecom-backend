ecd2234ade808d0a5aef6807942e3f0f6233d8ac
Aditi <aditisharma9104@gmail.com>
2025-09-26 15:07:27 +0530

    deploy

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-# Make sure the jar is in the same folder as start.sh
-java -jar dripyard-backend-0.0.1-SNAPSHOT.jar --server.port=$PORT
+java -jar dripyard-backend/target/dripyard-backend-0.0.1-SNAPSHOT.jar
